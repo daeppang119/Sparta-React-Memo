c47c162f6fc1f236f135302369f209fb9100bf70
daeppang119 <dbsk9436@naver.com>
2023-12-25 22:01:53 +0900

    심화 rtk

--- react-simhwa/rtk-counter/src/App.jsx
+++ react-simhwa/rtk-counter/src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNumber, minusNumber } from "./redux/modules/counterSlice";
 
 export default function App() {
-  return <div>App</div>;
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+
+  const handlerOnPluseBtnClick = () => {
+    dispatch(addNumber(1));
+  };
+
+  const handlerOnMinusBtnClick = () => {
+    dispatch(minusNumber(1));
+  };
+
+  return (
+    <div>
+      {number}
+      <button onClick={handlerOnPluseBtnClick}>+</button>
+      <button onClick={handlerOnMinusBtnClick}>-</button>
+    </div>
+  );
 }
